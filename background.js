c926002bcca997b2c3c7dddf0d9bb49ba176bb66
Tuchdov <tuchdov@gmail.com>
2024-09-21 20:04:34 +0300

    Add toggle functionality

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
+
 // Listen for installation or update of the extension
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Hibrish extension installed or updated');
 });
 
+let isEnabled = true;
 // Listen for clicks on the extension icon
 chrome.action.onClicked.addListener((tab) => {
+  isEnabled = !isEnabled;
+  updateIcon();
   // Toggle the extension on the current tab
   chrome.tabs.sendMessage(tab.id, { action: "toggle" });
 });
@@ -16,3 +20,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     sendResponse({ isEnabled: true });
   }
 });
+
+function updateIcon() {
+  const path = isEnabled ? {
+    "16": "icons/icon16.png",
+    "48": "icons/icon48.png",
+    "128": "icons/icon128.png"
+  } : {
+    "16": "icons/icon16-disabled.png",
+    "48": "icons/icon48-disabled.png",
+    "128": "icons/icon128-disabled.png"
+  };
+  
+  chrome.action.setIcon({ path: path });
+}
+
+// Set initial icon state
+updateIcon();
